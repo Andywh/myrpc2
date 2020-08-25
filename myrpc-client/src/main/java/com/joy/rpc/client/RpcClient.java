d4dc0363f2dbbd125520333e07082543dfd670d3
Andywh <529254699@qq.com>
2020-08-26 00:02:36 +0800

    去掉无用的代码

--- myrpc-client/src/main/java/com/joy/rpc/client/RpcClient.java
+++ myrpc-client/src/main/java/com/joy/rpc/client/RpcClient.java
@@ -2,10 +2,8 @@ package com.joy.rpc.client;
 
 import com.joy.rpc.client.connect.ChannelManager;
 import com.joy.rpc.client.connect.ConnectionManager;
-import com.joy.rpc.client.domain.AsyCallback;
 import com.joy.rpc.client.domain.Future;
 import com.joy.rpc.client.domain.context.FutureManager;
-import com.joy.rpc.client.handler.RpcClientHandler;
 import com.joy.rpc.client.proxy.RpcProxy;
 import com.joy.rpc.common.domain.Request;
 import io.netty.channel.*;
@@ -32,95 +30,6 @@ public class RpcClient {
     public static <T> T createService(Class<T> interfaceClass) {
         return RpcProxy.create(interfaceClass);
     }
-
-    //public void send(User user) {
-    //    EventLoopGroup group = new NioEventLoopGroup();
-    //    try {
-    //        // 创建并初始化 Netty 客户端 Bootstrap 对象
-    //        Bootstrap bootstrap = new Bootstrap();
-    //        bootstrap.group(group);
-    //        bootstrap.remoteAddress(new InetSocketAddress("127.0.0.1", 8894));
-    //        bootstrap.channel(NioSocketChannel.class);
-    //        bootstrap.handler(new ChannelInitializer<SocketChannel>() {
-    //            @Override
-    //            public void initChannel(SocketChannel channel) throws Exception {
-    //                ChannelPipeline pipeline = channel.pipeline();
-    //                pipeline.addLast(new RpcEncoder(User.class)); // 编码 RPC 请求
-    //                pipeline.addLast(new RpcDecoder(User.class)); // 解码 RPC 响应
-    //                pipeline.addLast(new RpcClientHandler()); // 处理 RPC 响应
-    //            }
-    //        });
-    //        bootstrap.option(ChannelOption.TCP_NODELAY, true);
-    //        // 连接 RPC 服务器
-    //        ChannelFuture future = bootstrap.connect("127.0.0.1", 8894).sync();
-    //        // 写入 RPC 请求数据并关闭连接
-    //        Channel channel = future.channel();
-    //        this.channel = channel;
-    //        //channel.writeAndFlush(new User("andy", 19));
-    //        channel.writeAndFlush(user).sync();
-    //        // fixme 下面一句注释了就变异步了,加上不要关闭连接（也就是注释掉 finally 里的代码)
-    //        //channel.closeFuture().sync();
-    //        // 返回 RPC 响应对象
-    //    } catch (Exception e) {
-    //        System.out.println("error: " + e);
-    //    }
-    //    //} finally {
-    //    //    System.out.println("client netty shut down");
-    //    //    group.shutdownGracefully();
-    //    //}
-    //}
-
-    //public Future sendRequest(User user) {
-    //    Future future = new Future(new Request());
-    //    try {
-    //        this.send(user);
-    //    } catch (Exception e) {
-    //
-    //    }
-    //    return future;
-    //}
-
-    //public void start() {
-    //    EventLoopGroup group = new NioEventLoopGroup();
-    //    try {
-    //        // 创建并初始化 Netty 客户端 Bootstrap 对象
-    //        Bootstrap bootstrap = new Bootstrap();
-    //        bootstrap.group(group);
-    //        bootstrap.remoteAddress(new InetSocketAddress("127.0.0.1", 8894));
-    //        bootstrap.channel(NioSocketChannel.class);
-    //        bootstrap.handler(new ChannelInitializer<SocketChannel>() {
-    //            @Override
-    //            public void initChannel(SocketChannel channel) throws Exception {
-    //                ChannelPipeline pipeline = channel.pipeline();
-    //                pipeline.addLast(new RpcEncoder(User.class)); // 编码 RPC 请求
-    //                pipeline.addLast(new RpcDecoder(User.class)); // 解码 RPC 响应
-    //                //pipeline.addLast(new RpcClientHandler()); // 处理 RPC 响应
-    //            }
-    //        });
-    //        bootstrap.option(ChannelOption.TCP_NODELAY, true);
-    //        // 连接 RPC 服务器
-    //        ChannelFuture future = bootstrap.connect("127.0.0.1", 8894).sync();
-    //        // 写入 RPC 请求数据并关闭连接
-    //        Channel channel = future.channel();
-    //        this.channel = channel;
-    //        //channel.writeAndFlush(new User("andy", 19));
-    //        channel.writeAndFlush(new User("andy", 30));
-    //        channel.closeFuture().sync();
-    //        // 返回 RPC 响应对象
-    //    } catch (Exception e) {
-    //        System.out.println("error: " + e);
-    //    } finally {
-    //        System.out.println("shut down");
-    //        group.shutdownGracefully();
-    //    }
-    //}
-
-    //public static Future send(Request request) {
-    //
-    //   return new Future() {
-    //   }
-    //
-    //}
 
     public Future send(Request request) {
         Future future = new Future(request);
@@ -157,29 +66,4 @@ public class RpcClient {
         System.out.println("end...");
     }
 
-    //public static void main(String[] args) throws ExecutionException, InterruptedException {
-    //    RpcClientHandler client = new RpcClientHandler();
-    //    //client.send(new User("an", 12));
-    //    //ChannelFuture channelFuture = client.channel.writeAndFlush(new User("And,", 90)).sync();
-    //    //RpcClient client = new RpcClient();
-    //    Request request = new Request();
-    //    request.setRequestId("2874362");
-    //    request.setClazzName("com.joy.rpc.UserServiceImpl");
-    //    request.setMethodName("query()");
-    //    Future future = client.sendRequest(request);
-    //    future.addCallBack(new AsyCallback() {
-    //        @Override
-    //        public void success(Object result) {
-    //            System.out.println("success");
-    //        }
-    //
-    //        @Override
-    //        public void fail(Exception e) {
-    //            System.out.println("fail");
-    //        }
-    //    });
-    //    System.out.println("continue...");
-    //    System.out.println("异步结果: " + future.get());
-    //    //System.out.println("end...");
-    //}
 }
--- myrpc-client/src/main/java/com/joy/rpc/client/handler/RpcClientHandler.java
+++ myrpc-client/src/main/java/com/joy/rpc/client/handler/RpcClientHandler.java
@@ -35,13 +35,11 @@ public class RpcClientHandler extends SimpleChannelInboundHandler<Response> {
         ChannelManager.putChannel("getUserName", ctx.channel());
     }
 
-
     @Override
     public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
         System.out.println("channelRegistered");
         super.channelRegistered(ctx);
     }
-
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, Response response) throws Exception {
@@ -50,51 +48,5 @@ public class RpcClientHandler extends SimpleChannelInboundHandler<Response> {
         future.done(response);
     }
 
-    public Future sendRequest(Request request) {
-        Future future = new Future(request);
-        FutureManager.putFuture(request.getRequestId(), future);
-        try {
-            this.send(request, future);
-        } catch (Exception e) {
-            System.out.println("error: " + e.toString());
-        }
-        return future;
-    }
-
-    public void send(Request request, Future future) {
-        EventLoopGroup group = new NioEventLoopGroup();
-        try {
-            // 创建并初始化 Netty 客户端 Bootstrap 对象
-            Bootstrap bootstrap = new Bootstrap();
-            bootstrap.group(group);
-            bootstrap.remoteAddress(new InetSocketAddress("127.0.0.1", 8894));
-            bootstrap.channel(NioSocketChannel.class);
-            bootstrap.handler(new ChannelInitializer<SocketChannel>() {
-                @Override
-                public void initChannel(SocketChannel channel) throws Exception {
-                    ChannelPipeline pipeline = channel.pipeline();
-                    pipeline.addLast(new RpcEncoder(Request.class)); // 编码 RPC 请求
-                    pipeline.addLast(new RpcDecoder(Response.class)); // 解码 RPC 响应
-                    pipeline.addLast(new RpcClientHandler()); // 处理 RPC 响应
-                }
-            });
-            bootstrap.option(ChannelOption.TCP_NODELAY, true);
-            // 连接 RPC 服务器
-            ChannelFuture future2 = bootstrap.connect("127.0.0.1", 8894).sync();
-            // 写入 RPC 请求数据并关闭连接
-            Channel channel = future2.channel();
-            //channel.writeAndFlush(new User("andy", 19));
-            channel.writeAndFlush(request).sync();
-            // fixme 下面一句注释了就变异步了,加上不要关闭连接（也就是注释掉 finally 里的代码)
-            //channel.closeFuture().sync();
-            // 返回 RPC 响应对象
-        } catch (Exception e) {
-            System.out.println("error: " + e);
-        }
-        //} finally {
-        //    System.out.println("client netty shut down");
-        //    group.shutdownGracefully();
-        //}
-    }
 
 }
